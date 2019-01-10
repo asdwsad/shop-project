f67980c76f47ebdd639e5b73b2fe622239e1a08a
Phantom Hacker <dattvse04864@fpt.edu.vn>
2019-01-10 13:53:49 +0700

    add trang admin

---
+++ ShopManageProject/Controllers/AdminController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using ShopManageProject.Models;
+using ShopManageProject.Models.CartItem;
+using ShopManageProject.Service;
+using PagedList;
+using System.Data.Entity;
+
+namespace ShopManageProject.Controllers
+{
+    
+
+    public class AdminController : Controller
+    {
+        private ShopManageDBEntities db = new ShopManageDBEntities();
+        private ProductsService productsService = new ProductsService();
+        private ProductCategoryService productCategoryService = new ProductCategoryService();
+
+        // GET: Admin
+        public ActionResult ProductList(int? page)
+        {
+            if (Session["Name"] == null||!Session["Name"].ToString().Equals("dat"))
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var product = productsService.ListProduct();
+
+            int pageNumber = (page ?? 1);
+
+            product = product.ToList().ToPagedList(pageNumber, 5);
+            ViewData["productList"] = product;
+            return View(product);
+        }
+
+
+        public ActionResult CreateProduct()
+        {
+            ViewBag.ProductCategoryId = new SelectList(db.ProductCategory, "ProductCategoryId", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateProduct([Bind(Include = "ProductId,Name,Code,Description,Image,Price,PromotionPrice,Quanlity,ProductCategoryId,CreateDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.CreateDate = DateTime.Now;
+                productsService.CreateProduct(product);
+                return RedirectToAction("ProductList");
+            }
+
+            ViewBag.ProductCategoryId = new SelectList(db.ProductCategory, "ProductCategoryId", "Name", product.ProductCategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult EditProduct(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductCategoryId = new SelectList(db.ProductCategory, "ProductCategoryId", "Name", product.ProductCategoryId);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProduct([Bind(Include = "ProductId,Name,Code,Description,Image,Price,PromotionPrice,Quanlity,ProductCategoryId,CreateDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.CreateDate = DateTime.Now;
+                productsService.editProduct(product);
+                //db.Entry(product).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("ProductList");
+            }
+            ViewBag.ProductCategoryId = new SelectList(db.ProductCategory, "ProductCategoryId", "Name", product.ProductCategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult DeleteProduct(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("DeleteProduct")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+           
+                db.Product.Remove(db.Product.Find(id));
+                db.SaveChanges();
+            
+            return RedirectToAction("ProductList");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //JsonResult
+    }
+}
+
+
+  
