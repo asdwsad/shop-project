5f94c9b59892b07f8e857b9b8a63016545ee165c
dattv29 <dattvse04864@fpt.edu.vn>
2019-01-11 10:22:17 +0700

    no message

--- ShopManageProject/Service/LoginService.cs
+++ ShopManageProject/Service/LoginService.cs
@@ -21,6 +21,21 @@ namespace ShopManageProject.Service
             }
         }
 
+        public int Check(UsersLogin u)
+        {
+            var us = userDAO.Login(u);
+            if (us != null)
+            {
+                if (us.UserName.Equals(u.UserName) && !us.Password.Equals(u.Password))
+                {
+                    return 0;
+                }
+                return 1;
+            }
+            
+            return 0;
+        }
+
         public Users Login(UsersLogin u)
         {
             return userDAO.Login(u);
